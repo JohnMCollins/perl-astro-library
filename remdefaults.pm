c67ef1d9150652c670cf6839f03bad69960e60eb
John M Collins <jmc@toad.me.uk>
2019-10-30 10:18:16 +0000

    Readjust remdefaults and add in env var.

--- remdefaults.pm
+++ remdefaults.pm
@@ -4,10 +4,11 @@ use Sys::Hostname;
 use Cwd;
 
 sub default_database {
+	return $ENV{'REMDB'} if defined $ENV{'REMDB'};
     my $hostn = hostname;
-    return "remfits" if $hostn eq "nancy" or $hostn eq "foxy";
+    return "remfits" if $hostn eq "lexi" or $hostn eq "nancy" or $hostn eq "foxy";
     return "cluster" if $hostn eq "uhhpc" or $hostn =~ /herts\.ac\.uK/;
-    "cluster";
+    "remfits";
 }
 
 sub get_tmpdir {
